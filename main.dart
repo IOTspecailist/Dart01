ceafc64eb1104b124b366f83653753b8e6a72bbc
nobody <asdf@asdffasdassadsad.com>
2023-01-24 15:19:29 +0900

    네임드 컨스트럭터

--- main.dart
+++ main.dart
@@ -1,21 +1,26 @@
 //Dart for Flutter
 void main() {
-  // 이건 constructor 초기화인거 같다 그래도 instance 생성되나보다
-  var player1 = Player("asdf", 1500);
+  var player1 = Player(
+    name: "asdf",
+    xp: 1500,
+  );
   player1.sayHellow();
-  var player2 = Player("sdds", 2500);
+  var player2 = Player(
+    name: "sdds",
+    xp: 2500,
+  );
   player2.sayHellow();
 }
 
 // * Class
 class Player {
-  // class 생성
   final String name;
   int xp;
-
-  // constructor 생성
-  // class와 이름이 같아야 한다
-  Player(this.name, this.xp);
+  // 중괄호를 추가해서 named constructor로 바꿨다
+  Player({
+    required this.name, // required를 추가해서 필수값으로 만들었다
+    required this.xp,
+  });
 
   void sayHellow() {
     print("HI my name is $name and i have $xp");
